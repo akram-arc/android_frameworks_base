440b19af8d00ef6eb1fe4a1d8eb997c5d527efad
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-10-23 23:44:43 -0400

    Hide power menu on secure lockscreen [1/2]
    Squashed:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sun, 29 Jan 2023 22:59:06 +0530
        Subject: Fix disabling power menu completely on secured locscreen
    
        * Fixes: https://github.com/crdroidandroid/issue_tracker/issues/107
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6734,6 +6734,12 @@ public final class Settings {
          */
 
         /**
+         * Whether to show power menu on LockScreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_ENABLE_POWER_MENU = "lockscreen_enable_power_menu";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
