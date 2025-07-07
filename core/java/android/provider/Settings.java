03b948ea313a412ffd35b2a549debc072b3c0422
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-07 11:59:23 +0800

    SystemUI: Add option to disable quicksettings on keyguard
    reverse-engineered from nothingOS - simplified
    
    Change-Id: Id2910cdc4e753609be3cbe7817605e27b5f6a53f
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13347,6 +13347,12 @@ public final class Settings {
         public static final String NAVBAR_IME_SPACE = "navbar_ime_space";
 
         /**
+         * Whether to allow swipe down on lockscreen to view Quick Panel
+         * @hide
+         */
+        public static final String ENABLE_LOCKSCREEN_QUICK_SETTINGS = "enable_lockscreen_quick_settings";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
